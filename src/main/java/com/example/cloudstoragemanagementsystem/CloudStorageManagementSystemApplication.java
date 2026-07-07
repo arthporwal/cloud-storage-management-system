763ee1d87f35f5259154a8c7b4181bd1c6f041e5
Arth <arthporwal556@gmail.com>
2026-07-07 21:45:59 +0530

    Integrate AWS S3 SDK and configure S3 client

--- src/main/java/com/example/cloudstoragemanagementsystem/CloudStorageManagementSystemApplication.java
+++ src/main/java/com/example/cloudstoragemanagementsystem/CloudStorageManagementSystemApplication.java
@@ -1,7 +1,10 @@
 package com.example.cloudstoragemanagementsystem;
 
+import com.example.cloudstoragemanagementsystem.service.S3Service;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CloudStorageManagementSystemApplication {
@@ -10,4 +13,10 @@ public class CloudStorageManagementSystemApplication {
         SpringApplication.run(CloudStorageManagementSystemApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner run(S3Service s3Service) {
+        return args -> {
+            s3Service.listObjects();
+        };
+    }
 }
---
+++ src/main/java/com/example/cloudstoragemanagementsystem/service/S3Service.java
@@ -0,0 +1,36 @@
+package com.example.cloudstoragemanagementsystem.service;
+
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
+import org.springframework.beans.factory.annotation.Value;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+
+import java.util.List;
+
+@Service
+public class S3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public List<String> listObjects() {
+
+        ListObjectsV2Request request = ListObjectsV2Request.builder()
+                .bucket(bucketName)
+                .build();
+
+        ListObjectsV2Response response = s3Client.listObjectsV2(request);
+
+        return response.contents()
+                .stream()
+                .map(object -> object.key())
+                .toList();
+    }
+}
