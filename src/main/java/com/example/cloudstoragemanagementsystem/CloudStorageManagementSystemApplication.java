9d007692d633054b9aaa4f4d71a1dbb6f04e3b89
Arth <arthporwal556@gmail.com>
2026-07-07 23:13:42 +0530

    Fix

--- src/main/java/com/example/cloudstoragemanagementsystem/CloudStorageManagementSystemApplication.java
+++ src/main/java/com/example/cloudstoragemanagementsystem/CloudStorageManagementSystemApplication.java
@@ -6,9 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class CloudStorageManagementSystemApplication {
 
+ // ssh -i ~/Downloads/aws-learning-key.pem ubuntu@<ip>
     public static void main(String[] args) {
         SpringApplication.run(CloudStorageManagementSystemApplication.class, args);
     }
@@ -16,7 +19,12 @@ public class CloudStorageManagementSystemApplication {
     @Bean
     CommandLineRunner run(S3Service s3Service) {
         return args -> {
-            s3Service.listObjects();
+
+            List<String> files = s3Service.listObjects();
+
+            for (String file : files) {
+                System.out.println(file);
+            }
         };
     }
 }
