5df3cee06d51721e2894edaf924c9a70ce4db3be
Arth <arthporwal556@gmail.com>
2026-07-07 19:58:55 +0530

    Initialize Spring Boot project

---
+++ src/main/java/com/example/cloudstoragemanagementsystem/CloudStorageManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudstoragemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStorageManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStorageManagementSystemApplication.class, args);
+    }
+
+}
