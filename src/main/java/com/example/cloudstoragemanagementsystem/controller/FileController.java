793be6f7e4ad9bf1f7d4fd6f49449597b5fbef6d
Arth <arthporwal556@gmail.com>
2026-07-16 22:54:50 +0530

    Upload files

--- src/main/java/com/example/cloudstoragemanagementsystem/controller/FileController.java
+++ src/main/java/com/example/cloudstoragemanagementsystem/controller/FileController.java
@@ -2,8 +2,12 @@ package com.example.cloudstoragemanagementsystem.controller;
 
 import com.example.cloudstoragemanagementsystem.service.S3Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -18,5 +22,10 @@ public class FileController {
     @GetMapping("/files")
     public List<String> listFiles() {
         return s3Service.listObjects();
+    }
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        return s3Service.uploadFile(file);
     }
 }
--- src/main/java/com/example/cloudstoragemanagementsystem/service/S3Service.java
+++ src/main/java/com/example/cloudstoragemanagementsystem/service/S3Service.java
@@ -2,11 +2,16 @@ package com.example.cloudstoragemanagementsystem.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class S3Service {
@@ -31,5 +36,27 @@ public class S3Service {
                 .stream()
                 .map(object -> object.key())
                 .toList();
+    }
+
+    public String uploadFile(MultipartFile file) throws IOException {
+
+        String fileName = file.getOriginalFilename();
+
+        String uniqueFileName = "users/101/" + UUID.randomUUID() + "-" + fileName;
+
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(uniqueFileName)
+                .build();
+
+        s3Client.putObject(
+                request,
+                RequestBody.fromInputStream(
+                        file.getInputStream(),
+                        file.getSize()
+                )
+        );
+
+        return uniqueFileName;
     }
 }
